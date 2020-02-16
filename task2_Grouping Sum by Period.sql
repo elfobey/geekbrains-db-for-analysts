327edb929547c0a8d8a15e99ef6fe735ebb016cf
mmingalov <mmingalov@gmail.com>
2020-02-16 14:26:17 +1000

    2 done

---
+++ task2_Grouping Sum by Period.sql
@@ -0,0 +1,11 @@
+﻿
+
+select 
+  CONCAT(
+    CAST(YEAR(o_date) as char), 
+    RIGHT(CONCAT('0',CAST(MONTH(o_date) as char)),2)) PERIOD
+  , sum(price) SUM
+  from orders_20190822
+group by CONCAT(
+    CAST(YEAR(o_date) as char), 
+    RIGHT(CONCAT('0',CAST(MONTH(o_date) as char)),2))
